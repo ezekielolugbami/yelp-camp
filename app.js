8498d7867b47d114c473f9d765e6ef32b2f4f5e8
Ezekiel Olugbami <ezekiel.olugbami@gmail.com>
2019-09-10 11:07:36 +0100

    Updated routes file

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg"},
+    {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__340.jpg"},
+    {name: "Solid Base", image: "https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906__340.jpg"}
+];
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {data: campgrounds});
+}); 
+
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to data array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect back to the campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
 });
 
 
